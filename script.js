8440cab66e1f341466978ec3acf1c529da00adac
Daria Neznaeva <darianeznaeva@gmail.com>
2022-08-29 21:36:11 +0300

    finish up website

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let weather = {
   // leaving the API key hard coded for demonstration purposes
   apiKey: "498ec749e72c5143ccc343309796b700",
-  fetchweather: function (city) {
+  fetchWeather: function (city) {
     fetch(
       "https://api.openweathermap.org/data/2.5/weather?q=" +
         city +
@@ -18,7 +18,7 @@ let weather = {
     const { icon, description } = data.weather[0];
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
-    console.log(name, icon, description, temp, humidity, speed);
+
     document.querySelector(".city").innerText = "Weather in " + name;
     document.querySelector(".icon").src =
       "https://openweathermap.org/img/wn/" + icon + ".png";
@@ -28,6 +28,24 @@ let weather = {
     document.querySelector(".humidity").innerText =
       "Humidity " + humidity + "%";
     document.querySelector(".wind").innerText = "Wind " + speed + " km/h";
+    document.querySelector(".weather").classList.remove("loading");
+    document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/1600x900/?" + name + "')";
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
   },
 };
-//https://api.openweathermap.org/data/2.5/weather?q={city name}&units=metric&&appid={API key}
+
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+});
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key === "Enter") {
+      weather.search();
+    }
+  });
+
+weather.fetchWeather("Berlin");
